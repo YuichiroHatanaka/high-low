e05affaf41d70f8b55d9cd51f2b40461e60d5fb8
YuichiroHatanaka <73636802+YuichiroHatanaka@users.noreply.github.com>
2021-03-31 23:50:07 +0900

    Create h&l.c

---
+++ h&l.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void){
+  printf("%d high or low ?\n", rand() % 13 + 1;);
+  
+  char a;
+  scanf("%c", &a);
+  
+  if(a == h){
+    if
+    
+  
+  return 0;
+}
